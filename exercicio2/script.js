44074fb6f899717fc6f3cf2d937df47f624f7bf3
MecMelt <eltonlimeira10@gmail.com>
2022-09-12 23:37:49 -0300

    exerciciolaço

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+// let num = Number(prompt ( " Escolha um número e saiba sua tabuáda de 1 a 10"))
+
+// for (index=num; index<= num*10; index+=num){
+//     console.log(index)
+// }
+
+let num = Number(prompt( "Escolha um número e saiba sua tabuáda de 0 a 10"))
+
+for(let i = 0; i<=num*10; i+=num){
+    console.log(i)
+}
